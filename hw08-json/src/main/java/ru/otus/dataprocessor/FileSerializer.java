a05927f74e4170be4cf95788630679a442453b4f
Abramov Alexey <abramov.saracin@yandex.ru>
2022-06-08 18:25:07 +0300

    hw7 remove @sneakythrows

--- hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -2,9 +2,9 @@ package ru.otus.dataprocessor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -13,9 +13,12 @@ public class FileSerializer implements Serializer {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final String fileName;
 
-    @SneakyThrows
     @Override
     public void serialize(Map<String, Double> data) {
-        objectMapper.writeValue(new File(fileName), data);
+        try {
+            objectMapper.writeValue(new File(fileName), data);
+        } catch (IOException exception) {
+            throw new FileProcessException("Error during writing file");
+        }
     }
 }
--- hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw08-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -3,10 +3,10 @@ package ru.otus.dataprocessor;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import ru.otus.dto.MeasurementDto;
 import ru.otus.model.Measurement;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,15 +16,18 @@ public class ResourcesFileLoader implements Loader {
     private final String fileName;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @SneakyThrows
     @Override
     public List<Measurement> load() {
         var sourceFile = getClass().getClassLoader().getResourceAsStream(fileName);
-        MappingIterator<MeasurementDto> iterator = objectMapper.readerFor(MeasurementDto.class)
+        try {
+            MappingIterator<MeasurementDto> iterator = objectMapper.readerFor(MeasurementDto.class)
                                                                .readValues(sourceFile);
-        return iterator.readAll().stream()
-                       .map(dto -> new Measurement(dto.getName(), dto.getValue()))
-                       .collect(Collectors.toList());
+            return iterator.readAll().stream()
+                           .map(dto -> new Measurement(dto.getName(), dto.getValue()))
+                           .collect(Collectors.toList());
+        } catch (IOException exception) {
+           throw new FileProcessException("Error during reading file");
+        }
     }
 
 }
