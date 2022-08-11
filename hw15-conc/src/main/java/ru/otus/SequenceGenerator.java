e78f01b4f59df1e51a57f47d12e8782459c485c8
Abramov Alexey <abramov.saracin@yandex.ru>
2022-08-11 10:13:29 +0300

    hw15 done

--- hw15-conc/src/main/java/ru/otus/SequenceGenerator.java
+++ hw15-conc/src/main/java/ru/otus/SequenceGenerator.java
@@ -5,40 +5,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 
 public class SequenceGenerator {
 
     private static final Logger logger = LoggerFactory.getLogger(SequenceGenerator.class);
 
-    private List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2);
-    private final Lock lock1 = new ReentrantLock();
-    private final Lock lock2 = new ReentrantLock();
+    private final List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2);
+    private final Semaphore semaphore1 = new Semaphore(1);
+    private final Semaphore semaphore2 = new Semaphore(0);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        var sequenceGenerator = new SequenceGenerator();
        sequenceGenerator.go();
     }
 
-    private void go() {
-        var thread1 = new Thread(() -> printValues(lock1, lock2));
-        var thread2 = new Thread(() -> printValues(lock2, lock1));
+    private void go() throws InterruptedException {
+        var thread1 = new Thread(() -> printValues(semaphore1, semaphore2));
+        var thread2 = new Thread(() -> printValues(semaphore2, semaphore1));
         thread1.start();
         thread2.start();
+        thread1.join();
+        thread2.join();
     }
 
-    private void printValues(Lock lock1, Lock lock2) {
+    private void printValues(Semaphore semaphore1, Semaphore semaphore2) {
         var iterator = Iterables.cycle(numbers).iterator();
-        try {
-            while (iterator.hasNext() && !Thread.currentThread().isInterrupted()) {
-                lock1.lock();
+        while (iterator.hasNext() && !Thread.currentThread().isInterrupted()) {
+            try {
+                semaphore1.acquire();
                 logger.info("{}", iterator.next());
-                lock1.unlock();
-                Thread.sleep(1_000);
+                semaphore2.release();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
         }
     }
 
